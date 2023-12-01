24c055be8d0a2182193219d5257aeacd0276dc0b
LucLind <100511857+LucLind@users.noreply.github.com>
2023-12-01 10:10:21 +0100

    Initial setup

---
+++ src/test/java/org/example/ArrayListTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class ArrayListTest {
+
+    @Test
+    public void test(){
+        System.out.println("Hello World!");
+    }
+}
