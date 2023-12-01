2d9d0d4e0aab5f5ed1e980e05e976804bd85c63e
LucLind <100511857+LucLind@users.noreply.github.com>
2023-12-01 11:14:14 +0100

    changed tests
    test how the array actually looks like instead of the values at an index

--- src/test/java/org/example/ArrayListTest.java
+++ src/test/java/org/example/ArrayListTest.java
@@ -23,7 +23,7 @@ public class ArrayListTest {
         list.add(1);
         array = list.getArray();
         assertThat(array.length, is(1));
-        assertThat(array[0], is(1));
+        assertThat(array, is(new int[]{1}));
     }
 
     @Test
@@ -32,9 +32,7 @@ public class ArrayListTest {
             list.add(i);
         }
 
-        for (int i = 0; i < 12; i++) {
-            assertThat(list.getArray()[i], is(i));
-        }
+        assertThat(list.getArray(), is(new int[]{0,1,2,3,4,5,6,7,8,9,10,11}));
     }
 
     @Test
